a3980b42b7b44417d454409341847b3804985a73
Mohamad Hossein Alahdadian <alahdadian@viratech.ir>
2020-06-04 20:32:22 +0430

    init

---
+++ phase1.py
@@ -0,0 +1,33 @@
+import pandas as pd
+# import numpy as np
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+# from sklearn.metrics import r2_score
+# from sklearn.metrics import mean_squared_error
+# from sklearn.metrics import max_error
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LinearRegression
+# from sklearn.metrics import r2_score
+# from sklearn.metrics import mean_squared_error
+# from sklearn.metrics import max_error
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# from sklearn.experimental import enable_iterative_imputer
+# import sklearn.impute as impute
+# import pandas_profiling as pp
+# from sklearn.linear_model import LinearRegression
+# from sklearn.metrics import r2_score
+# from sklearn.model_selection import train_test_split
+# from sklearn.cluster import DBSCAN
+# from scipy import stats
+# from sklearn.preprocessing import MinMaxScaler
+# import scipy as sp
+# from sklearn.preprocessing import RobustScaler
+# from mlxtend.frequent_patterns import apriori, association_rules
+# from scipy import sparse
+import pandas as pd
+
+# Create dataframes from json files
+df_test = pd.read_json('./Test_Data.json', lines=True)
+df_train = pd.read_json('./Train Data.json', lines=True)
+
